8cb1520b926632e0bdae7d6992f18d38162ed938
Andrew Marks <amracks@gmail.com>
2014-06-29 14:42:06 -0700

    set working dir for fetch, notify tomcat

--- manifests/init.pp
+++ manifests/init.pp
@@ -2,15 +2,16 @@ include tomcat7
 
 class jenkins {
 
-    $jenkins_url='http://mirrors.jenkins-ci.org/war/latest/jenkins.war'
-    $webapps_dir='/usr/local/apache-tomcat-7.0/webapps'
-
-    $execpath = '/bin:/sbin:/usr/bin:/usr/sbin'
+    $jenkins_url = 'http://mirrors.jenkins-ci.org/war/latest/jenkins.war'
+    $webapps_dir = '/usr/local/apache-tomcat-7.0/webapps'
+    $execpath    = '/bin:/sbin:/usr/bin:/usr/sbin'
 
     exec { 'jenkins-fetch':
         command => "fetch $jenkins_url",
         creates => "$webapps_dir/jenkins.war",
-        user => 'www',
-        group => 'www',
+        user    => 'www',
+        group   => 'www',
+        cwd     => "$webapps_dir",
+        notifiy => Class['tomcat7']
     }
 }
