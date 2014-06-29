039617ee23c3b1b1adc93f8d1995978c0053e0a0
Andrew Marks <amracks@gmail.com>
2014-06-29 14:35:03 -0700

    fetch latest jenkins war

---
+++ manifests/init.pp
@@ -0,0 +1,16 @@
+include tomcat7
+
+class jenkins {
+
+    $jenkins_url='http://mirrors.jenkins-ci.org/war/latest/jenkins.war'
+    $webapps_dir='/usr/local/apache-tomcat-7.0/webapps'
+
+    $execpath = '/bin:/sbin:/usr/bin:/usr/sbin'
+
+    exec { 'jenkins-fetch':
+        command => "fetch $jenkins_url",
+        creates => "$webapps_dir/jenkins.war",
+        user => 'www',
+        group => 'www',
+    }
+}
